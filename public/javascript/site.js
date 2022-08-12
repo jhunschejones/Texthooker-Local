387ac2b456b7c798aa0147cc8263bacdc9bd09bd
Joshua Hunsche Jones <jhunschejones@github.com>
2022-08-12 17:13:41 -0500

    Fix js bug, tighten up lines in css

--- public/javascript/site.js
+++ public/javascript/site.js
@@ -1,47 +1,43 @@
 (() => {
   document.addEventListener("DOMContentLoaded", () => {
+
     // The text inserter/scroller and the counter begin here.
-    let previousLinesCount = 0;
-    let charsCount = 0;
-
     document.addEventListener("DOMNodeInserted", () => {
-      // Counter begins here. Get the current number of lines first.
-      let currentLinesCount = document.getElementsByTagName("p").length;
 
-      // Second, confirm whether the node insertion was a new line.
-      // (Rikai also inserts and removes a node (a div).)
-      let newLinesCount = currentLinesCount - previousLinesCount;
+      // Counter begins here. Get the current number of lines first.
+      const currentLinesCount = document.getElementsByTagName("p").length;
+
+      // Next, confirm whether the node insertion was a new line.
+      // (Rikai also inserts and removes a div node.)
+      const previousLinesCount = document.querySelector("#counter").textContent.split(" / ")[1];
+      const newLinesCount = currentLinesCount - previousLinesCount;
       if (newLinesCount > 0) {
         // If it is a new line, do a character count of the line and add it to the running tally.
-        const newestLine = document.getElementsByTagName("p")[currentLinesCount - 1].textContent;
-        const newCharsCount = charsCount + newestLine.length;
+        const newestLine = document.getElementsByTagName("p")[currentLinesCount - 1];
+        const previousCarsCount = document.querySelector("#counter").textContent.split(" / ")[0];
+        const newCharsCount = previousCarsCount + newestLine.textContent.length;
 
         // Update the new counts in the counter.
         document.querySelector("#counter").textContent = `${newCharsCount.toLocaleString()} / ${currentLinesCount.toLocaleString()}`;
-
-        // Get ready for the next line.
-        previousLinesCount = currentLinesCount;
-        charsCount = newCharsCount;
 
         // The counter ends here and the text-scroller is below.
         // I've included it in the "if new line" statement.
         // (That is, it won't run unless a new line was added.)
         // Like this it won't autoscroll down every time Rikai is used.
 
-        var LEEWAY = 200; // Amount of "leeway" pixels before latching onto the bottom.
+        const LEEWAY = 200; // Amount of "leeway" pixels before latching onto the bottom.
 
         // Some obscene browser shit because making sense is for dweebs
-        var b = document.body;
-        var offset = b.scrollHeight - b.offsetHeight;
-        var scrollPos = (b.scrollTop+offset);
-        var scrollBottom = (b.scrollHeight - (b.clientHeight+offset));
+        const body = document.body;
+        const offset = body.scrollHeight - body.offsetHeight;
+        const scrollPosition = (body.scrollTop + offset);
+        const scrollBottom = (body.scrollHeight - (body.clientHeight + offset));
 
         // If we are at the bottom, go to the bottom again.
-        if (scrollPos >= scrollBottom - LEEWAY) {
-          window.scrollTo(0,document.body.scrollHeight);
+        if (scrollPosition >= scrollBottom - LEEWAY) {
+          window.scrollTo(0, document.body.scrollHeight);
         }
-
-      }; // This is the end of the "if new line" statement.
+      };
 
     }, false);
     // === End of scroller and counter script. ===
@@ -50,15 +46,15 @@
     // === Beginning of "remove last line" script. ===
     document.getElementById("removeButton").addEventListener("click", () => {
       // Check whether there are any lines to remove.
-      var linesToRemove = document.getElementsByTagName("p").length;
+      const linesToRemove = document.getElementsByTagName("p").length;
       if (linesToRemove > 0) {
-        const lastLine = document.getElementsByTagName("p")[currentLinesCount - 1].textContent;
+        const currentLinesCount = document.querySelector("#counter").textContent.split(" / ")[1];
+        console.log(currentLinesCount)
+        const lastLine = document.getElementsByTagName("p")[currentLinesCount - 1];
         document.querySelector("body").removeChild(lastLine);
-        document.querySelector("#counter").textContent = `${(charsCount - lastLine.length).toLocaleString()} / ${(currentLinesCount - 1).toLocaleString()}`;
 
-        // Prepare for next line.
-        previousLinesCount = currentLinesCount - 1;
-        charsCount = charsCount - lastLine.length;
+        const previousCarsCount = document.querySelector("#counter").textContent.split(" / ")[0];
+        document.querySelector("#counter").textContent = `${(previousCarsCount - lastLine.textContent.length).toLocaleString()} / ${(currentLinesCount - 1).toLocaleString()}`;
       };
     });
     // === End of "remove last line" script. ===
